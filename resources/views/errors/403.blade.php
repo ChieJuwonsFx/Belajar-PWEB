6ffd39bf25b4751f35bf088eb16f24b6cccd3a04
Richie Olajuwon Santoso <richieolajuwons@gmail.com>
2025-03-23 17:57:42 +0700

    update

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,4 +3,4 @@
 @section('title', __('Forbidden'))
 @section('code', '403')
 @section('message', __('Maaf, Anda tidak memiliki izin untuk mengakses halaman ini.'))
-@section('image', asset('assets/403.png'))
+@section('image', asset('assets/403.png'))   
